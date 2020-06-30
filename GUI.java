360da86890500beee1b326df8d7b42c63c6fde43
Wquarks <59671217+Wquarks@users.noreply.github.com>
2020-06-30 13:05:00 +0200

    rangement

---
+++ GUI.java
@@ -0,0 +1,143 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
+
+public class GUI  {
+
+	private static JFrame fenetre = new JFrame("Ekhos");
+	private static JPanel panel = new JPanel();
+	private static JTextField textField = new JTextField();
+	private static JButton bouton = new JButton("Chercher");
+	private static JLabel label = new JLabel("Sonorité :");
+	private static DefaultTableModel model = new DefaultTableModel();
+	private static JTable table = new JTable(model);
+	private static JMenuBar menuBar = new JMenuBar();
+	private static JMenu menu1 = new JMenu("Fichier");
+	private static JMenu menu2 = new JMenu("Edition");
+	private static JMenu menu3 = new JMenu("?");
+	private static JMenuItem save = new JMenuItem("save");
+	private static JMenuItem del = new JMenuItem("supprimer");
+	private static JMenuItem copier = new JMenuItem("copier");
+	private static JMenuItem coller = new JMenuItem("coller");
+	private static JMenuItem doc = new JMenuItem("doc");
+
+	public GUI() {
+		
+		fenetre.setJMenuBar(menuBar);
+		fenetre.setSize(500,400);
+		fenetre.setLocation(500,200);  	
+		fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenetre.add(panel,BorderLayout.CENTER);
+
+		textField.setColumns(10);
+
+		panel.add(textField);
+		panel.add(bouton);
+		panel.add(label);
+		panel.add(table);
+
+		model.addColumn("Mot");
+		model.addColumn("Sonorité");
+		model.addColumn("Points de similitude"); 
+
+		menu1.add(save);
+		menu2.add(del);
+		menu2.add(copier);
+		menu2.add(coller);
+		menu3.add(doc);
+		menuBar.add(menu1);
+		menuBar.add(menu2);
+		menuBar.add(menu3);
+
+		//addinlist("");
+		
+		
+		//tableau.setRowHeight(20);
+
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setPreferredSize(new Dimension(400, 280));
+		panel.add(scroll);
+
+		bouton.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e)
+			{
+				try {
+					Liste.init();
+				}catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+				if(!textField.getText().isEmpty()) {
+					String tf = textField.getText();
+
+					//flush
+					while( model.getRowCount() > 0) {
+						model.removeRow(0);
+					}
+
+					// mise en place du texte
+
+					Liste.search(tf);	
+					for(int y =0;y<Liste.arrayListMot.size();y++) {
+						model.addRow(new Object[]{Liste.arrayListMot.get(y)[0],Liste.arrayListMot.get(y)[1],Liste.arrayListMot.get(y)[2]});
+					}
+					espacevide();
+					label.setText("sonorité : "+Main.string2API(tf));
+				}
+			}
+		});	
+
+
+		del.addActionListener(new ActionListener() {		      
+			public void actionPerformed(ActionEvent arg0) {
+				while (table.getSelectedRow() != -1) {
+						model.removeRow(table.getSelectedRow());
+				}
+				espacevide();
+			}
+		});
+
+
+		espacevide();
+		color();
+		fenetre.setVisible(true);
+		
+	}
+	
+	public static void color(){
+		Color bc = new Color(52,152,219);
+		panel.setBackground(new Color(139,195,254));
+		textField.setBackground(Color.white);
+		bouton.setBackground(bc);
+		table.setBackground(Color.white);
+		table.getTableHeader().setBackground(bc);
+		menuBar.setBackground(bc);
+		table.getTableHeader().setForeground(Color.WHITE);
+		bouton.setForeground(Color.white);
+	}
+
+	
+
+	public static void espacevide() {
+		for(int u=model.getRowCount();u<=15;u++){
+			//String[] mot = {"","","0"};
+			//arrayListMot.add(arrayListMot.size(),mot);
+			model.addRow(new Object[]{"","","0"});
+		}
+	}
+}
---
+++ Liste.java
@@ -0,0 +1,134 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Liste {
+
+	protected static String fileName="yourFileName";
+	
+	protected static ArrayList<String[]> arrayListMot = new ArrayList<String[]>(); 
+	
+	public static void addinlist(String txt) throws IOException {
+		FileWriter fw=new FileWriter(fileName, true);    
+        fw.write("\n"+txt);    
+        fw.close();
+	}
+	
+	public static void search(String s){
+		for(int i=0; i<arrayListMot.size();i++) {
+			int sim = nbsimilarite(Main.string2API(s),arrayListMot.get(i)[1]);
+			arrayListMot.get(i)[2]=sim+"";
+		}
+
+		//on met la liste dans l'ordre des similarité décroisante
+		tri();
+
+		for(int i=0; i<arrayListMot.size();i++) {
+			String motlist = arrayListMot.get(i)[1];
+			arrayListMot.get(i)[2]=nbsimilarite(Main.string2API(s),motlist)+"";
+			//System.out.println(s+" vs "+arrayListMot.get(i)[0]+"		"+arrayListMot.get(i)[2]);
+		}
+
+	}
+	
+	/** Methode de tri de l'arrayliste du plus petit au plus grand en fonction du nb de similarité
+	 * @param r le rang a supprimé
+	 */
+	public static void tri(){
+
+		//suppression de toute les similarité à un certain rang  => gain de temp dans le tri.
+		for(int i=0; i<arrayListMot.size();i++) {
+			if (Integer.parseInt(arrayListMot.get(i)[2])<1) {
+				arrayListMot.remove(i);
+				i--;
+			}
+		}
+
+		boolean correction = true;
+		while (correction==true)  {
+			correction=false;
+			for(int n=1; n<arrayListMot.size();n++) {
+				if ( Integer.parseInt(arrayListMot.get(n-1)[2]) < Integer.parseInt(arrayListMot.get(n)[2])) {
+					arrayListMot.add(0,arrayListMot.get(n));
+					arrayListMot.remove(n+1);
+					correction = true;
+				}	
+			}
+		}
+	}
+	
+	/**
+	 * @param a le mot à comparer
+	 * @param b les mots de la liste
+	 * @return le point de similarité
+	 */
+	public static int nbsimilarite(String a,String b) { //compare les API des 2 mot
+		int similarite=0;
+		ArrayList<Character> prisEnCompte = new ArrayList<Character>();
+		prisEnCompte.add(' ');
+
+		int enchainement=0;
+		for(int aa=1; aa<a.length();aa++) {	
+			int soustour=0;
+			for(int bb=1; bb<b.length();bb++) {
+
+				//suite de 2 son
+				if (a.charAt(aa-1)==b.charAt(bb-1) && a.charAt(aa)==b.charAt(bb) ){
+					soustour++;
+					//System.out.println(a.charAt(aa-1)+""+a.charAt(aa)+"=="+b.charAt(bb-1)+""+b.charAt(bb)+"#"+enchainement+soustour+similarite); // sonorité semblable
+					similarite+=2;
+
+				}else{
+					//System.out.println(a.charAt(aa-1)+""+a.charAt(aa)+"!="+b.charAt(bb-1)+""+b.charAt(bb)+"_"+enchainement+soustour+similarite);
+				}
+
+				// son present dans le mot
+				if (a.charAt(aa)==b.charAt(bb)) {
+					soustour++;
+					//System.out.println(a.charAt(aa)+"=="+b.charAt(bb)+"#"+enchainement+soustour+similarite); // sonorité semblable
+					boolean aajouter=true;
+					for(char pec: prisEnCompte) {
+						if (a.charAt(aa)== pec){ 
+							aajouter=false;
+						}
+					}
+					if (aajouter==true){ 
+						prisEnCompte.add(a.charAt(aa));
+						similarite++;
+					}
+				}
+
+
+
+			}
+			if(soustour>0) {
+				similarite+=enchainement++;
+			}else {
+				enchainement=0;
+				similarite--;
+			}
+
+		}		
+		similarite-=b.length()-a.length();
+		return similarite;
+	}
+	
+	
+	@SuppressWarnings("resource")
+	public static void init() throws IOException {
+		arrayListMot.clear();
+		BufferedReader reader = new BufferedReader(new FileReader(fileName));
+		String s;
+		while( (s = reader.readLine()) != null) {
+			String[] mot = new String[3]; // mot[0] => mot de la liste; mot[1]=> mot transformé; mot[2]=> similarité
+			String str = new String(s.getBytes(),"UTF-8");
+
+			mot[0]=str;
+			mot[1]=Main.string2API(str);
+			mot[2]="0";	
+			arrayListMot.add(mot);
+		}
+	}
+}
