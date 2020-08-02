93171ea64e6c7a0bfc75dfd6a08e965285c7855e
Wquarks <59671217+Wquarks@users.noreply.github.com>
2020-08-02 23:51:15 +0200

    Update fonctionnalité MenuBar

--- GUI.java
+++ GUI.java
@@ -17,6 +17,7 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -47,19 +48,19 @@ public class GUI {
 	private static JMenu menu1 = new JMenu("Fichier");
 	private static JMenu menu2 = new JMenu("Edition");
 	private static JMenu menu3 = new JMenu("?");
-	private static JMenuItem save = new JMenuItem("save");
-	private static JMenuItem del = new JMenuItem("supprimer");
-	private static JMenuItem copier = new JMenuItem("copier");
-	private static JMenuItem coller = new JMenuItem("coller");
+	private static JMenuItem add = new JMenuItem("Ajouter un nouveau mot");
+	private static JMenuItem del = new JMenuItem("Supprimer un mot");
+	private static JMenuItem flush = new JMenuItem("Réinitialiser la liste");
 	private static JMenuItem doc = new JMenuItem("Code source");
-	private static ButtonGroup bg=new ButtonGroup();  
-	
-	protected static JRadioButton rb0 = new JRadioButton("Mot semblable");
-	protected static JRadioButton rb1 = new JRadioButton("Similarité sonore");
-	
-	
+	private static JMenuItem help = new JMenuItem("Aide");
+
+	private static ButtonGroup bg=new ButtonGroup();  	
 	private static Color cf = new Color(52,152,219); //couleur foncer
 	private static Color cc = new Color(139,195,254); //couleur claire 
+
+	protected static JRadioButton rb0 = new JRadioButton("Mot semblable");
+	protected static JRadioButton rb1 = new JRadioButton("Similarité sonore");
+
 
 	public GUI() {
 
@@ -71,13 +72,13 @@ public class GUI {
 		fenetre.add(panel0,BorderLayout.CENTER);
 
 		menuBar.setBorder(null);
-		
+
 		textField.setColumns(10);
 		textField.setMaximumSize(new Dimension(100000000,30));
 		textField.setFocusable(true);
 		textField.addKeyListener(listener);
 		textField.setBorder(new LineBorder(cf,2)) ;
-		
+
 		panel1.setBorder(new LineBorder(cc,2));
 		panel2.setBorder(new LineBorder(cc,2));
 		panel3.setBorder(new LineBorder(cc,2));
@@ -87,18 +88,18 @@ public class GUI {
 		panel0.add(panel3);
 		panel0.add(panel4);
 		panel0.setBorder(new LineBorder(cc,15)) ;
-		
+
 		panel0.setLayout(new BoxLayout(panel0, BoxLayout.PAGE_AXIS));
 		panel1.setLayout(new BoxLayout(panel1, BoxLayout.LINE_AXIS));
 		panel2.setLayout(new BoxLayout(panel2, BoxLayout.LINE_AXIS));
 		panel3.setLayout(new BoxLayout(panel3, BoxLayout.LINE_AXIS));
 		panel4.setLayout(new BoxLayout(panel4, BoxLayout.LINE_AXIS));
-		
-		
+
+
 		bg.add(rb0);
 		bg.add(rb1);	
 		rb0.setSelected(true);
-		
+
 		panel1.add(textField);
 		panel1.add(bouton);
 		panel2.setLayout(new BorderLayout());
@@ -109,53 +110,96 @@ public class GUI {
 
 		table.addKeyListener(listener);
 		table.setBorder(null);
-		
+
 		model.addColumn("Mot");
 		model.addColumn("Sonorité");
 		model.addColumn("Points de similitude"); 
-		 
-		
-		menu1.add(save);
+
+
+		menu1.add(flush);
+		menu2.add(add);
 		menu2.add(del);
-		menu2.add(copier);
-		menu2.add(coller);
+		menu3.add(help);
 		menu3.add(doc);
 		menuBar.add(menu1);
 		menuBar.add(menu2);
 		menuBar.add(menu3);
-
 
 		//tableau.setRowHeight(20);
 
 		JScrollPane scroll = new JScrollPane(table);
 		scroll.setBackground(cc);
 		scroll.setBorder(new LineBorder(cc,0));
-		
+
 		panel4.add(scroll);
+
+		add.addActionListener(new ActionListener() {		      
+			public void actionPerformed(ActionEvent arg0) {
+				String motAAdd= JOptionPane.showInputDialog(null, "Veuilliez saisir le mot à ajouter", "Ajouter", JOptionPane.QUESTION_MESSAGE);
+				System.out.println("Mot à add : "+ motAAdd);
+				try {
+					Liste.addinlist(motAAdd);
+					actionChercher();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}); 
+
+		flush.addActionListener(new ActionListener() {		      
+			public void actionPerformed(ActionEvent arg0) {
+				int option = JOptionPane.showConfirmDialog(null,"Etes vous sûr de vouloir réinitialiser la liste ?","Réinitialisation", JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+				if(option == JOptionPane.OK_OPTION){
+					Main.file.delete();
+					Liste.start();
+				}
+			}
+		}); 
+		
+		help.addActionListener(new ActionListener() {		      
+			public void actionPerformed(ActionEvent arg0) {
+				JOptionPane.showMessageDialog(null, "Voici quelque astuces : \n "
+						+ "- La touche ENTRER pour lancer la recherche \n"
+						+ "- La touche SUPPR pour suprimmer un mot dans la liste (cela le retirera juste de la recherche actuelle) \n"
+						+ "\n"
+						+ "Pour information : \n "
+						+ "- La liste contient actuellement : "+Liste.nbMots()+" mots \n"
+						+ "- Auteur : Wquarks \n"
+						+ "- Version : "+Main.version, "Aide",JOptionPane.INFORMATION_MESSAGE);
+						
+			}
+		}); 
 
 		bouton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				actionChercher();
-				}
+			}
 		});	
-		
+
 		rb0.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				actionChercher();
-				}
+			}
 		});	
 		rb1.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e){
 				actionChercher();
-				}
+			}
 		});	
 
 
 		del.addActionListener(new ActionListener() {		      
 			public void actionPerformed(ActionEvent arg0) {
-				actionDel();
+				String motADel= JOptionPane.showInputDialog(null, "Veuilliez saisir le mot à supprimer", "Supprimer", JOptionPane.QUESTION_MESSAGE);
+				System.out.println("Mot à del : "+ motADel);
+				try {
+					Liste.delinlist(motADel);
+					actionChercher();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-		});
+		}); 
 
 		doc.addActionListener(new ActionListener() {		      
 			public void actionPerformed(ActionEvent arg0) {
@@ -169,6 +213,9 @@ public class GUI {
 		color();
 		fenetre.setVisible(true);
 
+
+
+
 	}
 
 
@@ -179,7 +226,7 @@ public class GUI {
 		}
 		espacevide();
 	}
-	public void actionChercher() {
+	public static void actionChercher() {
 		try {
 			Liste.init();
 		}catch (IOException e1) {
@@ -198,21 +245,21 @@ public class GUI {
 
 			Liste.search(tf);	
 			for(int y =0;y<Liste.arrayListMot.size();y++) {
-				model.addRow(new Object[]{Liste.arrayListMot.get(y)[0],Liste.arrayListMot.get(y)[1],Liste.arrayListMot.get(y)[2]});
+				model.addRow(new Object[]{Liste.arrayListMot.get(y)[0],Main.patchAPI(Liste.arrayListMot.get(y)[1]),Liste.arrayListMot.get(y)[2]});
 			}
 			espacevide();
-		
+
 			String word = Main.string2API(tf);
 			if(word.equals("")) {
 				word="None";
 			}
-			label.setText("Sonorité : ["+word+"]");
+			label.setText("Sonorité : ["+Main.patchAPI(word)+"]");
 		}
 	}
 
 
 	public static void color(){
-		
+
 		fenetre.setBackground(cc);
 		panel0.setBackground(cc);
 		panel1.setBackground(cc);
@@ -221,7 +268,12 @@ public class GUI {
 		panel4.setBackground(cc);
 		rb0.setBackground(cc);
 		rb1.setBackground(cc);
-		
+
+
+		menu1.setForeground(Color.white);
+		menu2.setForeground(Color.white);
+		menu3.setForeground(Color.white);
+
 		textField.setBackground(Color.white);
 		bouton.setBackground(cf);
 		table.setBackground(Color.white);
@@ -232,7 +284,7 @@ public class GUI {
 	}
 
 	public static void espacevide() {
-		for(int u=model.getRowCount();u<15;u++){
+		for(int u=model.getRowCount();u<14;u++){
 			model.addRow(new Object[]{"","","0"});
 		}
 	}
@@ -243,7 +295,7 @@ public class GUI {
 		//touche enfoncée
 		public void keyPressed(KeyEvent e) {
 			Codetouche=e.getKeyCode();
-		//System.out.println(e.getKeyCode());
+			//System.out.println(e.getKeyCode());
 		}
 
 		//touche relevée
